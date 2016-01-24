19ad5f866e45f9cef8875c957fe46507beefddc2
梁賀翔 <joellykimo@gmail.com>
2016-01-24 22:05:33 +0800

    Initial Commit

---
+++ iPhone_PadTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhone_PadTest
+//
+//  Created by sean.liang on 2016/1/24.
+//  Copyright © 2016年 sean.liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
