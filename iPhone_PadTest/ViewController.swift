f283ca0c3f7d1d142b177852023b2aada791facc
seanliang <sean.liang@gmail.com>
2016-08-01 23:03:50 +0800

    version 1 test

--- iPhone_PadTest/ViewController.swift
+++ iPhone_PadTest/ViewController.swift
@@ -19,6 +19,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        var alert = UIAlertController(title: "登入" , message: "請輸入帳號密碼" ,preferredStyle: UIAlertControllerStyle.Alert)
+        
+        
+        var okBtn = UIAlertAction(title: "登入", style: UIAlertActionStyle.Default , handler: { ( action : UIAlertAction!) -> Void in
+        })
+        
+        var cancel = UIAlertAction(title: "取消", style:UIAlertActionStyle.Default, handler: {(action : UIAlertAction) -> Void in})
+        
+        alert.addTextFieldWithConfigurationHandler { (textField :UITextField) -> Void in textField.placeholder = "Login"}
+        alert.addTextFieldWithConfigurationHandler { (textField :UITextField) -> Void in textField.placeholder = "password"}
+        
+        alert.addAction(okBtn)
+        alert.addAction(cancel)
+        self.presentViewController(alert,animated: true,completion: nil)
+    }
+    
 
 
 }
